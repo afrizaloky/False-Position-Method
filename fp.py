707d0110f3671846b4d0d80a55f96281d926bcc9
afrizaloky <36653795+afrizaloky@users.noreply.github.com>
2019-05-04 16:43:01 +0700

    Update fp.py

--- fp.py
+++ fp.py
@@ -9,31 +9,31 @@ from prettytable import PrettyTable
 x = var('x')
 
 try :
-	user_input = input("Enter the function (3*x**2+2*x+2) = ")	
-	expr = sympify(user_input)
+        user_input = input("Enter the function (3*x**2+2*x+2) = ")      
+        expr = sympify(user_input)
 except:
-	print("input yang anda masukkan tidak sesuai")
-	sys.exit()
-	
+        print("input yang anda masukkan tidak sesuai")
+        sys.exit()
+        
 try:
-	xl = float(input("Enter the value of Xl (-10) = "))	
+        xl = float(input("Enter the value of Xl (-10) = "))     
 except:
-	print("input yang anda masukkan tidak sesuai")
-	sys.exit()
-	
+        print("input yang anda masukkan tidak sesuai")
+        sys.exit()
+        
 
 try:
-	xu = float(input("enter the value of xu (15) = "))
+        xu = float(input("enter the value of xu (15) = "))
 except:
-	print("input yang anda masukkan tidak sesuai")
-	sys.exit()
-	
+        print("input yang anda masukkan tidak sesuai")
+        sys.exit()
+        
 
 try:
-	RAE = float(input("Enter the value of tollerance (0.0001) = "))
+        RAE = float(input("Enter the value of tollerance (0.0001) = "))
 except:
-	print("input yang anda masukkan tidak sesuai")
-	sys.exit()
+        print("input yang anda masukkan tidak sesuai")
+        sys.exit()
 
 RAE_now = 100
 counter = 0
@@ -41,47 +41,52 @@ x_old = 0
 
 #Value from function
 def f(value):
-	
-	res = expr.subs(x, value)
-	return res
+        
+        res = expr.subs(x, value)
+        return res
 
 
 #False Position Method
 def false_position(xu, xl):
-	atas = xu*f(xl)-xl*f(xu)
-	bawah = f(xl)-f(xu)
-	xm = atas/bawah
-	return xm
+        atas = xu*f(xl)-xl*f(xu)
+        bawah = f(xl)-f(xu)
+        xm = atas/bawah
+        return xm
 
 t = PrettyTable()
 t.field_names=["i", "xi", "Tollerance"]
 
 try:
-	while RAE<RAE_now:
+        while RAE<RAE_now:
 
-	
-		xm = false_position(xl, xu)
-		diff=f(xl)*f(xm)
-		if(diff<0):
-			xu=xm
-		elif(diff>0):
-			xl=xm
-		else:
-			print("True Value")
-			break
-		if(counter==0):
-			x_new = xm
-			t.add_row([counter, x_new, "-"])
-			counter=counter+1
-			x_old=x_new
-			continue
-		x_new = xm
-		selisih = abs(x_new-x_old)
-		RAE_now = abs(selisih/x_new)
-		t.add_row([counter, x_new, RAE_now])
-		x_old=x_new
-		counter=counter+1
+        
+                xm = false_position(xl, xu)
+                diff=f(xl)*f(xm)
+                if(diff<0):
+                        xu=xm
+                elif(diff>0):
+                        xl=xm
+                elif(diff==0):
+                        print("True Value = "+str(xm)+".")
+                        break
+                        sys.exit()
+                else:
+                        print("Program Error")
+                        break
+                        sys.exit()
+                if(counter==0):
+                        x_new = xm
+                        t.add_row([counter, x_new, "-"])
+                        counter=counter+1
+                        x_old=x_new
+                        continue
+                x_new = xm
+                selisih = abs(x_new-x_old)
+                RAE_now = abs(selisih/x_new)
+                t.add_row([counter, x_new, RAE_now])
+                x_old=x_new
+                counter=counter+1
 except:
-	print("Program Error")
-	sys.exit()
+        print("Program Error")
+        sys.exit()
 print(t)
