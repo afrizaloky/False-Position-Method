6dcba6a57f4dd5c944e419dfa7c8ed8db6807536
afrizaloky <36653795+afrizaloky@users.noreply.github.com>
2019-05-04 16:17:26 +0700

    first upload

---
+++ fp.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+from sympy import var
+from sympy import sympify
+from math import *
+import sys
+from prettytable import PrettyTable
+
+#Input
+x = var('x')
+
+try :
+	user_input = input("Enter the function (3*x**2+2*x+2) = ")	
+	expr = sympify(user_input)
+except:
+	print("input yang anda masukkan tidak sesuai")
+	sys.exit()
+	
+try:
+	xl = float(input("Enter the value of Xl (-10) = "))	
+except:
+	print("input yang anda masukkan tidak sesuai")
+	sys.exit()
+	
+
+try:
+	xu = float(input("enter the value of xu (15) = "))
+except:
+	print("input yang anda masukkan tidak sesuai")
+	sys.exit()
+	
+
+try:
+	RAE = float(input("Enter the value of tollerance (0.0001) = "))
+except:
+	print("input yang anda masukkan tidak sesuai")
+	sys.exit()
+
+RAE_now = 100
+counter = 0
+x_old = 0
+
+#Value from function
+def f(value):
+	
+	res = expr.subs(x, value)
+	return res
+
+
+#False Position Method
+def false_position(xu, xl):
+	atas = xu*f(xl)-xl*f(xu)
+	bawah = f(xl)-f(xu)
+	xm = atas/bawah
+	return xm
+
+t = PrettyTable()
+t.field_names=["i", "xi", "Tollerance"]
+
+try:
+	while RAE<RAE_now:
+
+	
+		xm = false_position(xl, xu)
+		diff=f(xl)*f(xm)
+		if(diff<0):
+			xu=xm
+		elif(diff>0):
+			xl=xm
+		else:
+			print("True Value")
+			break
+		if(counter==0):
+			x_new = xm
+			t.add_row([counter, x_new, "-"])
+			counter=counter+1
+			x_old=x_new
+			continue
+		x_new = xm
+		selisih = abs(x_new-x_old)
+		RAE_now = abs(selisih/x_new)
+		t.add_row([counter, x_new, RAE_now])
+		x_old=x_new
+		counter=counter+1
+except:
+	print("Program Error")
+	sys.exit()
+print(t)
---
+++ readme.txt
@@ -0,0 +1,4 @@
+how to use
+
+pip install -r requirements.txt
+python fp.py or ./fp.py
