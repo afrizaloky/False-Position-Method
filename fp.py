05f9bf43d23aabd401a5b87d23dc44a9bef0bf5a
afrizaloky <afrizal.d.s@mail.ugm.ac.id>
2019-05-25 10:35:50 +0700

    update repo

--- fp.py
+++ fp.py
@@ -4,99 +4,100 @@ from sympy import sympify
 from math import *
 import sys
 from prettytable import PrettyTable
+import time
 
 
-
-
-#Value from function
+# Value from function
 def f(value):
-        
-        res = expr.subs(x, value)
-        return res
+
+    res = expr.subs(x, value)
+    return res
 
 
-#False Position Method
+# False Position Method
 def false_position(xu, xl):
-        atas = xu*f(xl)-xl*f(xu)
-        bawah = f(xl)-f(xu)
-        xm = atas/bawah
-        return xm
+    atas = xu*f(xl)-xl*f(xu)
+    bawah = f(xl)-f(xu)
+    xm = atas/bawah
+    return xm
 
-#Input
+
+# Input
 x = var('x')
 
-try :
-        user_input = input("Enter the function (3*x**2+2*x+2) = ")      
-        expr = sympify(user_input)
-except:
-        print("input yang anda masukkan tidak sesuai")
-        sys.exit()
-        
 try:
-        xl = float(input("Enter the value of Xl (-10) = "))     
+    user_input = input("Enter the function (3*x**2+2*x+2) = ")
+    expr = sympify(user_input)
 except:
-        print("input yang anda masukkan tidak sesuai")
-        sys.exit()    
+    print("input yang anda masukkan tidak sesuai")
+    sys.exit()
 
 try:
-        xu = float(input("enter the value of xu (15) = "))
+    xl = float(input("Enter the value of Xl (-10) = "))
 except:
-        print("input yang anda masukkan tidak sesuai")
-        sys.exit()
-        
+    print("input yang anda masukkan tidak sesuai")
+    sys.exit()
+
+try:
+    xu = float(input("enter the value of xu (15) = "))
+except:
+    print("input yang anda masukkan tidak sesuai")
+    sys.exit()
+
 if(f(xl)*f(xu) < 0):
-	success = 1
+    success = 1
 else:
-	print("Pastikan nilai F(xl) dan F(xu) berlawanan tanda")
-	sys.exit()
+    print("Pastikan nilai F(xl) dan F(xu) berlawanan tanda")
+    sys.exit()
 
 try:
-        RAE = float(input("Enter the value of Aproximation Error (0.0001) = "))
+    RAE = float(input("Enter the value of Aproximation Error (0.0001) = "))
 except:
-        print("input yang anda masukkan tidak sesuai")
-        sys.exit()
+    print("input yang anda masukkan tidak sesuai")
+    sys.exit()
 
 RAE_now = 100
 counter = 0
 x_old = 0
 
-#Definition Table
+# Definition Table
 t = PrettyTable()
-t.field_names=["i", "xi", "Aproximation Error"]
+t.field_names = ["i", "xi", "Aproximation Error"]
 
-
+start = time.time()
 try:
-        #looping
-        while RAE<RAE_now:
+    # looping
+    while RAE < RAE_now:
 
-        
-                xm = false_position(xl, xu)
-                diff=f(xl)*f(xm)
-                if(diff<0):
-                        xu=xm
-                elif(diff>0):
-                        xl=xm
-                elif(diff==0):
-                        print("True Value = "+str(xm)+".")
-                        sys.exit()
-                else:
-                        print("Program Error")
-                        sys.exit()
-                if(counter==0):
-                        x_new = xm
-                        t.add_row([counter, x_new, "-"])
-                        counter=counter+1
-                        x_old=x_new
-                        continue
-                x_new = xm
-                selisih = abs(x_new-x_old)
-                RAE_now = abs(selisih/x_new)
-                t.add_row([counter, x_new, RAE_now])
-                x_old=x_new
-                counter=counter+1
+        xm = false_position(xl, xu)
+        diff = f(xl)*f(xm)
+        if(diff < 0):
+            xu = xm
+        elif(diff > 0):
+            xl = xm
+        elif(diff == 0):
+            print("True Value = "+str(xm)+".")
+            sys.exit()
+        else:
+            print("Program Error")
+            sys.exit()
+        if(counter == 0):
+            x_new = xm
+            t.add_row([counter, x_new, "-"])
+            counter = counter+1
+            x_old = x_new
+            continue
+        x_new = xm
+        selisih = abs(x_new-x_old)
+        RAE_now = abs(selisih/x_new)
+        t.add_row([counter, x_new, RAE_now])
+        x_old = x_new
+        counter = counter+1
 except:
-        print("Program Error")
-        sys.exit()
+    print("Program Error")
+    sys.exit()
+end = time.time()
 
 
 print(t)
+print("Waktu yang dibutuhkan adalah "+str(end-start)+" detik")
